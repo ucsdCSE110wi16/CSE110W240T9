1ccbbdcb885ad8dc06b66d82ae615fc929647bf0
SeanJia <jzw_sw@126.com>
2016-02-04 15:21:12 -0800

    more basic classes

---
+++ EventListInputStream.java
@@ -0,0 +1,24 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class EventListInputStream {
+
+	ObjectInputStream objIn; 
+	
+	public EventListInputStream(String filename) throws IOException {
+		FileInputStream fi = new FileInputStream(filename);
+		this.objIn = new ObjectInputStream(fi);     
+	}
+	
+	public CalendarEventList readList() {
+		CalendarEventList result = null; 
+		try {
+			result = (CalendarEventList) this.objIn.readObject();
+		} catch (ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+		}
+		
+	    return result;
+	}
+}
