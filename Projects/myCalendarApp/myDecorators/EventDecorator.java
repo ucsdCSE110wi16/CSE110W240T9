4d685da2b1d000ed5ce68092569e51b7566dc1c4
will.jiang <will.jiang@servicenow.com>
2016-02-09 17:36:39 -0800

    added methods to event decorator

--- Projects/myCalendarApp/myDecorators/EventDecorator.java
+++ Projects/myCalendarApp/myDecorators/EventDecorator.java
@@ -1,9 +1,3 @@
-package com.prolificinteractive.materialcalendarview.sample.decorators;
-
-import com.prolificinteractive.materialcalendarview.CalendarDay;
-import com.prolificinteractive.materialcalendarview.DayViewDecorator;
-import com.prolificinteractive.materialcalendarview.DayViewFacade;
-import com.prolificinteractive.materialcalendarview.spans.DotSpan;
 
 import java.util.Collection;
 import java.util.HashSet;
@@ -13,5 +7,22 @@ import java.util.HashSet;
  */
 public class EventDecorator implements DayViewDecorator {
 
+    private int color;
+    private HashSet<CalendarDay> dates;
+
+    public EventDecorator(int color, Collection<CalendarDay> dates) {
+        this.color = color;
+        this.dates = new HashSet<>(dates);
+    }
+
+    @Override
+    public boolean shouldDecorate(CalendarDay day) {
+        return dates.contains(day);
+    }
+
+    @Override
+    public void decorate(DayViewFacade view) {
+        view.addSpan(new DotSpan(5, color));
+    }
 
 }
