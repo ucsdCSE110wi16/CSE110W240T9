24ead4a0607f1d0b6774f0151470d36bf7019c1e
will.jiang <will.jiang@servicenow.com>
2016-02-09 17:27:26 -0800

    Add event decorator

---
+++ Projects/myCalendarApp/myDecorators/EventDecorator.java
@@ -0,0 +1,17 @@
+package com.prolificinteractive.materialcalendarview.sample.decorators;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.spans.DotSpan;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+/**
+ * Decorate several days with a dot
+ */
+public class EventDecorator implements DayViewDecorator {
+
+
+}
