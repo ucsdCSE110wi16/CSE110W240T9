10390e8084fc5b199b9987ed27d7b13ec7b6cf16
will.jiang <will.jiang@servicenow.com>
2016-02-16 16:39:15 -0800

    Added methods to MySelectorDecorator.java

--- myCalenderApp/myDecoratorsWilliam/MySelectorDecorator.java
+++ myCalenderApp/myDecoratorsWilliam/MySelectorDecorator.java
@@ -1,4 +1,19 @@
 public class MySelectorDecorator implements DayViewDecorator {
 
+    private final Drawable myDrawable;
+
+    public MySelectorDecorator(Activity myActivity) {
+        myDrawable = myActivity.getResources().getDrawable(R.myDrawable.my_selector);
+    }
+
+    @Override
+    public boolean shouldDecorate(CalendarDay myDay) {
+        return true;
+    }
+
+    @Override
+    public void decorate(DayViewFacade myDayView) {
+        myDayView.setSelectionDrawable(myDrawable);
+    }
 
 }
