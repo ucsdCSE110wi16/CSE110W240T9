030fbb0b5c1776e9408678cac9b9170d12a6e8aa
will.jiang <will.jiang@servicenow.com>
2016-02-20 16:40:56 -0800

    Changed drop down choices to Static or Dynamic

--- Alex/myCalenderApp/sample/src/main/java/com/prolificinteractive/materialcalendarview/sample/AddEventFormActivity.java
+++ Alex/myCalenderApp/sample/src/main/java/com/prolificinteractive/materialcalendarview/sample/AddEventFormActivity.java
@@ -33,8 +33,8 @@ public class AddEventFormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_form);
-        Spinner dropdown = (Spinner) findViewById(R.id.spinner1);
-        String[] items = new String[]{"Static", "Periodic"};
+        Spinner dropdown = (Spinner) findViewById(R.id.typeOfEvent);
+        String[] items = new String[]{"Static", "Dynamic"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
         dropdown.setAdapter(adapter);
 
