14e8cfe89134d31456bb373fc7b0af4bf084e671
AlexOuyang <ouyangchenxing@hotmail.com>
2016-02-21 05:09:50 -0800

    added material design

---
+++ Alex/myCalenderApp2/app/src/main/java/com/prolificinteractive/materialcalendarview/sample/MainActivity.java
@@ -0,0 +1,175 @@
+package com.prolificinteractive.materialcalendarview.sample;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+import com.prolificinteractive.materialcalendarview.sample.decorators.EventDecorator;
+import com.prolificinteractive.materialcalendarview.sample.decorators.OneDayDecorator;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.concurrent.Executors;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Main View is a calender
+ */
+public class MainActivity extends Activity implements OnDateSelectedListener {
+
+    private Toolbar toolbar;
+
+    private final OneDayDecorator oneDayDecorator = new OneDayDecorator();
+    private static final DateFormat FORMATTER = SimpleDateFormat.getDateInstance();
+
+    private Button addEvent;
+
+
+    @Bind(R.id.calendarView)
+    MaterialCalendarView widget;
+
+    @Bind(R.id.textView)
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        /* Add Toolbar */
+        toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
+//        setSupportActionBar(toolbar);
+
+        widget.setOnDateChangedListener(this);
+
+        /* Shows extra date for next month to align the dates */
+//        widget.setShowOtherDates(MaterialCalendarView.SHOW_ALL);
+
+        Calendar calendar = Calendar.getInstance();
+
+        /* Highlight today's date on create */
+        widget.setSelectedDate(calendar.getTime());
+
+        /* Uesd to limit how many month user can scroll through */
+        calendar.set(calendar.get(Calendar.YEAR), Calendar.JANUARY, 1);
+        widget.setMinimumDate(calendar.getTime());
+        calendar.set(calendar.get(Calendar.YEAR), Calendar.DECEMBER, 31);
+        widget.setMaximumDate(calendar.getTime());
+
+        /* Setup the selected date as the text */
+        textView.setText(getSelectedDatesString());
+
+        /* Decoration used to highlight the weekends */
+//        widget.addDecorators(new HighlightWeekendsDecorator());
+
+        new ApiSimulator().executeOnExecutor(Executors.newSingleThreadExecutor());
+
+        /* This is the add event button */
+        addEvent = (Button) findViewById(R.id.btnAddEvent);
+//        addEvent.setText("Add Event");
+        addEvent.setBackgroundResource(R.drawable.ic_add_event);
+        addEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddEventFormActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+        //If you change a decorate, you need to invalidate decorators
+        oneDayDecorator.setDate(date.getDate());
+        widget.invalidateDecorators();
+
+        // Change selected date on textview
+        textView.setText(getSelectedDatesString());
+    }
+
+    /**
+     * Simulate an API call to show how to add decorators
+     */
+    private class ApiSimulator extends AsyncTask<Void, Void, List<CalendarDay>> {
+
+        @Override
+        protected List<CalendarDay> doInBackground(@NonNull Void... voids) {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Calendar calendar = Calendar.getInstance();
+            calendar.add(Calendar.MONTH, -2);
+            ArrayList<CalendarDay> dates = new ArrayList<>();
+            for (int i = 0; i < 30; i++) {
+                CalendarDay day = CalendarDay.from(calendar);
+                dates.add(day);
+                calendar.add(Calendar.DATE, 5);
+            }
+
+            return dates;
+        }
+
+        @Override
+        protected void onPostExecute(@NonNull List<CalendarDay> calendarDays) {
+            super.onPostExecute(calendarDays);
+
+            if (isFinishing()) return;
+            // Decorate the date event dots with color #FF4646
+            widget.addDecorator(new EventDecorator(Color.parseColor("#FF4646"), calendarDays));
+        }
+    }
+
+    // Used to get the selected date as a string
+    private String getSelectedDatesString() {
+        CalendarDay date = widget.getSelectedDate();
+        if (date == null)
+            return "Wrong Date Selection";
+        else
+            return FORMATTER.format(date.getDate());
+    }
+
+}
