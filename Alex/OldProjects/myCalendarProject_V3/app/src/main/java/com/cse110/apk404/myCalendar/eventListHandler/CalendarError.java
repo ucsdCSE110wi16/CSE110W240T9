8308611e52df8dddfe9461807d54735ad40335a5
AlexOuyang <ouyangchenxing@hotmail.com>
2016-02-24 18:58:34 -0800

    /Useadded android:launchMode=singleTop in MainActivity to make sure MainActivity's calendar view is resumed instead of restarted once you go back from detailed description page after clicking on a event slot in the calender view,  also reorganized old proejcts into a folder named OldProjects

---
+++ Alex/OldProjects/myCalendarProject_V3/app/src/main/java/com/cse110/apk404/myCalendar/eventListHandler/CalendarError.java
@@ -0,0 +1,12 @@
+package com.cse110.apk404.myCalendar.eventListHandler;
+
+
+public class CalendarError extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public CalendarError(String message) {
+		super(message);
+	}
+
+}
