939b2a4d4297b97f65645175c57f16eafe35a097
kristinetran14 <kristinekhanhtran@gmail.com>
2016-02-29 02:34:40 -0800

    fixed file organization and add finished event lists

--- CSE110W240T9-master-2/Alex/OldProjects/myCalendarApp_V1/app/src/main/java/com/prolificinteractive/materialcalendarview/sample/SettingsActivity.java
+++
@@ -1,8 +0,0 @@
-package com.prolificinteractive.materialcalendarview.sample;
-
-/**
- * Created by chenxingouyang on 2/21/16.
- */
-public class SettingsActivity {
-
-}
--- CSE110W240T9-master-2/Algorithm/CalenderSort/src/CalendarEventList.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-
-public interface CalendarEventList {
-	public ArrayList<StaticEvent> getList();
-	public void setList(ArrayList<StaticEvent> list);
-	public boolean addEvent(StaticEvent event) throws CalendarError;
-}
