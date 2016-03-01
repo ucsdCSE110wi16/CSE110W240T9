e1660a59419fe35fd24419d1789595720e3023d3
AlexOuyang <ouyangchenxing@hotmail.com>
2016-03-01 03:04:56 -0800

    fixed merge conflicts

--- Alex/myCalendarProject/app/src/main/java/com/cse110/apk404/myCalendar/DetailActivity.java
+++ Alex/myCalendarProject/app/src/main/java/com/cse110/apk404/myCalendar/DetailActivity.java
@@ -141,7 +141,14 @@ public class DetailActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to tool bar if it is present.
         getMenuInflater().inflate(R.menu.activity_detail, menu);
 
-                menu.findItem(R.id.action_edit_event).setVisible(false);
+        // Limie menu items
+        if (event.isFinished()) {
+            menu.findItem(R.id.action_edit_event).setVisible(false);
+            menu.findItem(R.id.action_finish_event).setVisible(false);
+        } else {
+            menu.findItem(R.id.action_edit_event).setVisible(false);
+            menu.findItem(R.id.action_unfinish_event).setVisible(false);
+        }
 
         return true;
     }
